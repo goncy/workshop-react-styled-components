94c79a42914ae35d29841df3dc3560bfbe787077
goncy <gonzalo.pozzo4@gmail.com>
2019-05-14 14:35:46 -0300

    Base

--- app/src/App.js
+++ app/src/App.js
@@ -6,15 +6,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
